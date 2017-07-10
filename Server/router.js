d28a3b1ae8eec8414e7d049f787e7fd457c02506
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-10 17:04:38 -0600

    fix case sensitivity of GET by country requests.  add POST country and malnutrition routes/api functions

--- Server/router.js
+++ Server/router.js
@@ -8,4 +8,8 @@ router.get('/malnutrition_data', api.getOneResource);
 router.get('/countries/malnutrition_data/:name', api.getCountryMalnutritionData);
 router.get('/yearly/malnutrition_data/:year', api.getYearlyMalnutritionData);
 
+router.post('/countries', api.postNewCountry);
+router.post('/malnutrition_data', api.postNewMalnutritionData);
+
+
 module.exports = router;
