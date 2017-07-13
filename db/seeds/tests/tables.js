433f901e9763a44f25ed297bfe14a07f51310f4f
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-12 23:59:31 -0600

    happy and sad path tests complete for get malnutrition data by country

--- db/seeds/tests/tables.js
+++ db/seeds/tests/tables.js
@@ -3,9 +3,9 @@ const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
 const countryData = [
-  { id: 1, name: 'smeland', iso_code: 'sme', region: 'jungleland', income_group: 'no-money' },
-  { id: 2, name: 'yuvaland', iso_code: 'yuv', region: 'coconut-trees', income_group: 'rich' },
-  { id: 3, name: 'dexland', iso_code: 'dex', region: 'bushes', income_group: 'bones' },
+  { id: 1, name: 'SMELAND', iso_code: 'sme', region: 'jungleland', income_group: 'no-money' },
+  { id: 2, name: 'YUVALAND', iso_code: 'yuv', region: 'coconut-trees', income_group: 'rich' },
+  { id: 3, name: 'DEXLAND', iso_code: 'dex', region: 'bushes', income_group: 'bones' },
 ];
 
 const countriesData = (knex) => {
@@ -22,9 +22,9 @@ const countriesData = (knex) => {
 };
 
 const malnutritionData = [
-  { id: 1, country_name: 'smeland', year: '1980', under_5_population: '3000', sample_size: '300', severe_wasting: '30', wasting: '3', overweight: '10', stunting: '20', underweight: '30' },
-  { id: 2, country_name: 'yuvaland', year: '1984', under_5_population: '9000', sample_size: '900', severe_wasting: '90', wasting: '9', overweight: '90', stunting: '90', underweight: '90' },
-  { id: 3, country_name: 'dexland', year: '2017', under_5_population: '6000', sample_size: '600', severe_wasting: '60', wasting: '6', overweight: '60', stunting: '60', underweight: '60' },
+  { id: 1, country_name: 'SMELAND', year: '1980', under_5_population: '3000', sample_size: '300', severe_wasting: '30', wasting: '3', overweight: '10', stunting: '20', underweight: '30' },
+  { id: 2, country_name: 'YUVALAND', year: '1984', under_5_population: '9000', sample_size: '900', severe_wasting: '90', wasting: '9', overweight: '90', stunting: '90', underweight: '90' },
+  { id: 3, country_name: 'DEXLAND', year: '2017', under_5_population: '6000', sample_size: '600', severe_wasting: '60', wasting: '6', overweight: '60', stunting: '60', underweight: '60' },
 ];
 
 const malnutritionsData = (knex) => {
