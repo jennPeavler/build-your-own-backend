b9f6517577452af63562cdc428c25aa015ba0e73
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-13 20:32:22 -0600

    check in place for missing data in a POST request.  Will not allow missing data for required POST country info or POST malnutrition info

--- Server/postRequests.js
+++ Server/postRequests.js
@@ -5,15 +5,21 @@ const database = require('knex')(configuration);
 const postNewCountry = (req, res) => {
   const { region, income_group } = req.body;
   let { name, iso_code } = req.body;
-  name = name.toUpperCase();
-  iso_code = iso_code.toUpperCase();
+  const reqParams = ['name', 'iso_code', 'region', 'income_group'];
+  const hasAllData = reqParams.every(param => req.body[param]);
 
-  database('countries').insert({ name, iso_code, region, income_group }, 'id')
-  .then((countryIndex) => {
-    countryIndex.length ? res.status(201).send('Country recorded in table')
-    : res.status(422).send('Unable to record country into table, check if data entered into every column and every column exist in table');
-  })
-  .catch(error => res.status(500).send(error));
+  if (hasAllData) {
+    name = name.toUpperCase();
+    iso_code = iso_code.toUpperCase();
+    database('countries').insert({ name, iso_code, region, income_group }, 'id')
+    .then((countryIndex) => {
+      countryIndex.length ? res.status(201).send('Country recorded in table')
+      : res.status(422).send('Unable to record country into table, check if column exist in table');
+    })
+    .catch(error => res.status(500).send(error));
+  } else {
+    res.status(422).send('Unable to record country into table.  Missing data in post request.');
+  }
 };
 
 const postNewMalnutritionData = (req, res) => {
@@ -26,23 +32,30 @@ const postNewMalnutritionData = (req, res) => {
           stunting,
           underweight } = req.body;
   let { country_name } = req.body;
-  country_name = country_name.toUpperCase();
 
-  database('malnutrition_data').insert(
-    { country_name,
-      year,
-      under_5_population,
-      sample_size,
-      severe_wasting,
-      wasting,
-      overweight,
-      stunting,
-      underweight }, 'id')
-  .then((dataIndex) => {
-    dataIndex.length ? res.status(201).send('Malnutrition data recorded in database')
-    : res.status(422).send('Unable to record malnutrition data into database, check if the data column exists');
-  })
-  .catch(error => res.status(500).send(error));
+  const reqParams = ['country_name', 'year'];
+  const hasAllData = reqParams.every(param => req.body[param]);
+
+  if (hasAllData) {
+    country_name = country_name.toUpperCase();
+    database('malnutrition_data').insert(
+      { country_name,
+        year,
+        under_5_population,
+        sample_size,
+        severe_wasting,
+        wasting,
+        overweight,
+        stunting,
+        underweight }, 'id')
+    .then((dataIndex) => {
+      dataIndex.length ? res.status(201).send('Malnutrition data recorded in database')
+      : res.status(422).send('Unable to record malnutrition data into database, check if the data column exists');
+    })
+    .catch(error => res.status(500).send(error));
+  } else {
+    res.status(422).send('Unable to record malnutrition data into table.  Missing data in post request.');
+  }
 };
 
 module.exports = {
