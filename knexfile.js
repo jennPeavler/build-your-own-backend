7fcdaab6c61057c8dc82f8b6272be44de4aecaba
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-13 21:35:11 -0600

    added circle.yml and edited the test connection in the knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/child_malnutrition_test',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/child_malnutrition_test',
     migrations: {
       directory: './db/migrations/tests',
     },
