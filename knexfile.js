da590cf69fec5759be4ca44337def532b504f81a
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-13 23:04:02 -0600

    add process.DATABASE_URL to knex test connection.  makes tests fail locally, but hoping it will work for circleCI

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/child_malnutrition_test' || 'postgresql://ubuntu:@127.0.0.1:5432/circle_test',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/child_malnutrition_test',
     migrations: {
       directory: './db/migrations/tests',
     },
